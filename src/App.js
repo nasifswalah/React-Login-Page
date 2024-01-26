4d28d12220d50d3aeb3c6c184884f51f6958afe5
nasifswalah <nasifswalah@gmail.com>
2024-01-26 23:11:24 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='bg-img' src='https://img.freepik.com/free-vector/realistic-style-technology-particle-background_23-2148426704.jpg?size=626&ext=jpg&ga=GA1.1.1448711260.1706227200&semt=ais'/>
+      <div className='login'>
+        <input className='input' id='name' placeholder='Email'></input>
+        <input id='name' className='input' placeholder='Password'></input>
+        <p className='new'>Create new account <p className='signup new'>Singup</p></p>
+        <button className='login-btn'>Login</button>
+      </div>
     </div>
   );
 }
-
 export default App;
